6087b5fc84ec9c9826117ed53ab2edd1ed775393
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-06-09 18:46:41 +0630

    [Complete] GlobalUsings.cs

---
+++ DotNet8WebApi.ODataSample/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿// Global using directives
+
+global using System.Reflection.Metadata;
+global using DotNet8WebApi.ODataSample.Database;
+global using DotNet8WebApi.ODataSample.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.OData;
+global using Microsoft.AspNetCore.OData.Deltas;
+global using Microsoft.AspNetCore.OData.Formatter;
+global using Microsoft.AspNetCore.OData.Query;
+global using Microsoft.AspNetCore.OData.Results;
+global using Microsoft.AspNetCore.OData.Routing.Controllers;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.OData.Edm;
+global using Microsoft.OData.ModelBuilder;
