9af2fa0da07d17901a9c1c240b13b6fa1381bb0b
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-06-09 18:47:20 +0630

    [Complete] BlogController.cs

---
+++ DotNet8WebApi.ODataSample/Features/Blog/BlogController.cs
@@ -0,0 +1,70 @@
+﻿namespace DotNet8WebApi.ODataSample.Features.Blog;
+
+public class BlogsController : ODataController
+{
+    private readonly AppDbContext _context;
+
+    public BlogsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [EnableQuery]
+    public IQueryable<BlogModel> Get()
+    {
+        return _context.Blogs;
+    }
+
+    [EnableQuery]
+    public SingleResult<BlogModel> Get([FromODataUri] int key)
+    {
+        var result = _context.Blogs.Where(b => b.BlogId == key);
+        return SingleResult.Create(result);
+    }
+
+    public async Task<IActionResult> Post([FromBody] BlogModel blog)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        _context.Blogs.Add(blog);
+        await _context.SaveChangesAsync();
+
+        return Created(blog);
+    }
+
+    public async Task<IActionResult> Patch([FromODataUri] int key, [FromBody] Delta<BlogModel> blog)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var existingBlog = await _context.Blogs.FindAsync(key);
+        if (existingBlog == null)
+        {
+            return NotFound();
+        }
+
+        blog.Patch(existingBlog);
+        await _context.SaveChangesAsync();
+
+        return Updated(existingBlog);
+    }
+
+    public async Task<IActionResult> Delete([FromODataUri] int key)
+    {
+        var blog = await _context.Blogs.FindAsync(key);
+        if (blog == null)
+        {
+            return NotFound();
+        }
+
+        _context.Blogs.Remove(blog);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
