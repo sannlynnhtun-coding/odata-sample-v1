a876183f1ef819407ced2a6f5054f7e8685491ae
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-06-09 18:47:07 +0630

    [Complete] AppDbContext.cs

---
+++ DotNet8WebApi.ODataSample/Database/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNet8WebApi.ODataSample.Database;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
